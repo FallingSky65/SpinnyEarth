86007a838feaf63603836919f7b32a2c8e70a53d
Daniel Li <danielgongli.65@gmail.com>
2024-06-14 14:08:30 -0400

    fixed frames/hi.txt

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 rm *.png
-rm ./frames/*
+rm ./frames/*.png
 ./bin/SpinnyEarth
 mv *.png ./frames/
 ffmpeg -framerate 30 -pattern_type glob -i 'frames/*.png' -c:v libx264 -pix_fmt yuv420p renders/result.mp4
